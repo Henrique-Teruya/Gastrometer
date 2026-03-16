2e9b07079619f52b92d66698b3a6095861865e8f
Henrique Teruya <henriqueteruya12@gmail.com>
2026-03-16 15:14:43 -0300

    dawd

--- nextjs-start/src/lib/firebase/serverApp.js
+++ nextjs-start/src/lib/firebase/serverApp.js
@@ -1,14 +1,22 @@
-// enforces that this code can only be called on the server
-// https://nextjs.org/docs/app/building-your-application/rendering/composition-patterns#keeping-server-only-code-out-of-the-client-environment
-import "server-only";
-
-import { cookies } from "next/headers";
-import { initializeServerApp, initializeApp } from "firebase/app";
-
+// src/lib/firebase/serverApp.js
+import { initializeServerApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { cookies } from "next/headers";
+import { initializeApp } from "firebase/app";
 
-// Returns an authenticated client SDK instance for use in Server Side Rendering
-// and Static Site Generation
 export async function getAuthenticatedAppForUser() {
-  throw new Error("not implemented");
+  const authIdToken = (await cookies()).get("__session")?.value;
+
+  const firebaseServerApp = initializeServerApp(
+    // Configurações do seu app
+    initializeApp(),
+    {
+      authIdToken,
+    }
+  );
+
+  const auth = getAuth(firebaseServerApp);
+  await auth.authStateReady();
+
+  return { firebaseServerApp, currentUser: auth.currentUser };
 }
